44ecb0d00a7e73a1cf0ef6bfb3fdda887910a1ea
lilakos <31805615+lilakos@users.noreply.github.com>
2018-06-01 14:59:39 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+echo "Uncomment the 9th line of this file to create a properly sized disk image"
+echo "Uncomment lines 14-15 for disassembly"
+
+nasm -f bin -o boot.bin bootloader.asm
+nasm -f bin -o stage2.bin stage2.asm
+nasm -f bin -o staticindex.bin staticindex.asm
+
+#dd if=LilakOS.img of=/dev/zero bs=512 count=2097152
+dd if=boot.bin of=LilakOS.img bs=512 seek=0 conv=notrunc
+dd if=stage2.bin of=LilakOS.img bs=512 seek=1 conv=notrunc
+dd if=staticindex.bin of=LilakOS.img bs=512 seek=2097151 conv=notrunc
+
+ndisasm boot.bin>bootloader.dis
+ndisasm stage2>stage2.dis
+
+echo "Done!"
