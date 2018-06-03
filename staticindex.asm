dfb16d99be25a730555221777b0ab96241c3de82
lilakos <31805615+lilakos@users.noreply.github.com>
2018-06-03 11:57:40 -0600

    Update staticindex.asm
    Kernel Image

--- staticindex.asm
+++ staticindex.asm
@@ -10,9 +10,9 @@ times 320-($-$$) db 0x00
 db 0x12 ;Type
 db 0x00 ;Continuation Entries
 dq 0x00 ;Timestamp
-dq 0x01 ;Start block
-dq 0x03 ;End block
-dq 1024 ;File Size
+dq 0x03 ;Start block
+dq 0x16 ;End block
+dq 9952 ;File Size
 db '/boot/LilakOSKernelImage.LEF', 0x0
 times 384-($-$$) db 0x00
 db 0x12 ;Type
