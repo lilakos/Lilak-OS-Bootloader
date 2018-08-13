36249d7edced08511c6d1174ec83b3a1ea4bf134
lilakos <31805615+lilakos@users.noreply.github.com>
2018-08-13 01:30:21 -0600

    Update staticindex.asm

--- staticindex.asm
+++ staticindex.asm
@@ -1,31 +1,25 @@
-;
-; SFS Filesystem because fuck FAT!
-; But I don't have any utility to create images for me
-; I don't care though
-;
-
-times 256-($-$$) db 0x00
-db 0x02 ;Type
-times 320-($-$$) db 0x00
-db 0x12 ;Type
-db 0x00 ;Continuation Entries
-dq 0x00 ;Timestamp
-dq 0x03 ;Start block
-dq 0x16 ;End block
-dq 9952 ;File Size
-db '/boot/LilakOSKernelImage.LEF', 0x0
-times 384-($-$$) db 0x00
-db 0x12 ;Type
-db 0x00 ;Continuation Entries
-dq 0x00 ;Timestamp
-dq 0x01 ;Start block
-dq 0x03 ;End block
-dq 1024 ;File Size
-db '/boot/stage2.LEF', 0x0
-times 448-($-$$) db 0x00
-db 0x01 ;Type
-db 0x00 ;Zero
-db 0x00 ;Zero
-db 0x00 ;Zero
-dq 0x00 ;Timestamp
-db ' ~ LilakOS v1.0 ~ ', 0x00
+times 0x80-($-$$) db 0
+db 0x02
+times 0xC0-($-$$) db 0
+db 0x12
+db 0x1
+dq 0x0
+dq 0x4
+dq 0x40
+dq 0x7000
+db "/LilakOS/core/LOS32BitKernel.LEF"
+times 0x140-($-$$) db 0
+db 0x12
+db 0x1
+dq 0x0
+dq 0x1
+dq 0x3
+dq 0x600
+db "/LilakOS/Bootstuff/stage2/stage2.LBF", 0x00
+times 0x1C0-($-$$) db 0
+db 1
+db 0
+db 0
+db 0
+dq 0x0
+db "LilakOS 2GB HDD Image Copyright (c) 2018,Jacob Smith"
