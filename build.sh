3ef454081e13dc9b19cb2f45192bc5bc001aa440
lilakos <31805615+lilakos@users.noreply.github.com>
2018-08-13 01:29:39 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ nasm -f bin -o boot.bin bootloader.asm
 nasm -f bin -o stage2.bin stage2.asm
 nasm -f bin -o staticindex.bin staticindex.asm
 
-#dd if=LilakOS.img of=/dev/zero bs=512 count=2097152
+#dd if=LilakOS.img of=/dev/zero bs=512 count=‭4194304‬
 dd if=boot.bin of=LilakOS.img bs=512 seek=0 conv=notrunc
 dd if=stage2.bin of=LilakOS.img bs=512 seek=1 conv=notrunc
 dd if=staticindex.bin of=LilakOS.img bs=512 seek=2097151 conv=notrunc
